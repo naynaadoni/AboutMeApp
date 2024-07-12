f4853c7c7a1227f0a6a902b3f316c2c5cf54f4d3
Nayna Adoni <sunayna.adoni.17@gmail.com>
2024-07-12 13:52:01 -0500

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Sunayna Adoni on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
---
+++ AboutMeApp/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  AboutMeApp
+//
+//  Created by Sunayna Adoni on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+            ZStack {
+                Color(.systemBlue)
+                    .ignoresSafeArea()
+                VStack(alignment: .center, spacing: 20.0) {
+                    Image("me")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    Text("Sunayna Adoni")
+                        .foregroundColor(Color.blue)
+                    Text("Hi!! I'm so excited to be here. I'm having so much fun coding but if you want to learn more about me, keep scrolling!")
+                        .foregroundColor(Color.blue)
+                   
+                    
+                    HStack {
+                        VStack {
+                            Image("pitbull")
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
+                            Text("Concert-Lover")
+                                .foregroundColor(Color.blue)
+                                .multilineTextAlignment(.center)
+                        }
+                        VStack {
+                            Image("food")
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
+                            
+                            Text("Food-Lover")
+                                .foregroundColor(Color.blue)
+                                .multilineTextAlignment(.center)
+                        }
+                       
+                    }
+                    HStack {
+                        Image("lake")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
+                        Text("Lake Girl & Duck Lover")
+                            .foregroundColor(Color.blue)
+                    }
+                    HStack {
+                        Text("Me & My Bsf")
+                            .foregroundColor(Color.blue)
+                        Image("bo")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(/*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/)
+                    }
+
+                    Image("pitbull")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                        
+                    Text("Putting My Concert Pic in Here Again Because it was PITBULL!!!")
+                        .foregroundColor(Color.blue)
+                        .multilineTextAlignment(.center)
+                        
+                    
+                }
+                .padding()
+                .background(Rectangle())
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius: 15)
+                .padding()
+                    
+                }
+                
+            }
+           
+
+       
+        }
+    }
+
+
+#Preview {
+    ContentView()
+}
